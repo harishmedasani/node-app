2f807905a50e6aa73630079afc3bae26154c1ae8
harishmedasani <49905524+harishmedasani@users.noreply.github.com>
2019-08-02 13:37:51 +0530

    Update deploycontainer.sh

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -6,4 +6,7 @@ if [ $node_app=='nodeapp' ]; then
     echo "nodeapp is running, lets delete"
         docker rm -f nodeapp
 fi
-docker run -d -p 8080:8080 --name=nodeapp harishmedasani/node:1.0
+
+images=`docker images | grep kammana/nodejenkins | awk '{print $3}'`
+docker rmi $images
+docker run -d -p 8080:8080 --name nodeapp $1
